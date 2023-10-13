1509be619940449c5c14e5b26dec77f927cd82af
cxs <chengxiaosan@gmail.com>
2023-10-13 19:29:30 +0800

    i18n

---
+++ app/src/main/java/configgen/tool/GenI18n.java
@@ -0,0 +1,74 @@
+package configgen.tool;
+
+import configgen.gen.*;
+import configgen.gen.I18n;
+import configgen.util.SheetData;
+import configgen.util.SheetHandler;
+import configgen.util.SheetUtils;
+import configgen.value.CfgValue;
+import configgen.value.ForeachPrimitiveValue;
+
+import java.io.File;
+import java.util.*;
+
+public final class GenI18n extends Generator  {
+
+    private final File file;
+    private final String encoding;
+
+    public GenI18n(Parameter parameter) {
+        super(parameter);
+        file = new File(parameter.get("file", "../i18n/i18n-config.csv", "生成文件"));
+        encoding = parameter.get("encoding", "GBK", "生成文件的编码");
+        parameter.end();
+    }
+
+    @Override
+    public void generate(Context ctx) {
+        table2TextMap.clear();
+        CfgValue cfgValue = ctx.makeValue(tag);
+
+        for (CfgValue.VTable vTable : cfgValue.sortedTables()) {
+            ForeachPrimitiveValue.foreachVTable(this::visit, vTable);
+        }
+
+
+        List<List<String>> rows = new ArrayList<>(64 * 1024);
+        for (Map.Entry<String, Map<String, String>> table2TextEntry : table2TextMap.entrySet()) {
+            String table = table2TextEntry.getKey();
+            for (Map.Entry<String, String> s : table2TextEntry.getValue().entrySet()) {
+                List<String> r = new ArrayList<>(2);
+                r.add(table);
+                r.add(s.getKey());
+                r.add(s.getValue());
+                rows.add(r);
+            }
+        }
+
+        SheetUtils.writeToFile(SheetData.valueOf(file, rows), encoding);
+    }
+
+    private void visit(CfgValue.PrimitiveValue primitiveValue, String table, List<String> fieldChain){
+
+
+    }
+
+    private final Map<String, Map<String, String>> table2TextMap = new TreeMap<>();
+    private String lastTable;
+    private Map<String, String> lastTextMap;
+
+    @Override
+    public void enterTable(String table) {
+        lastTable = table;
+        lastTextMap = null;
+    }
+
+    @Override
+    public void enterText(String original, String text) {
+        if (lastTextMap == null) {
+            lastTextMap = new LinkedHashMap<>();
+            table2TextMap.put(lastTable, lastTextMap);
+        }
+        lastTextMap.put(original, text);
+    }
+}
