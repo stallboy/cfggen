2ad8ba698c085e773e9441cc41aa1c04f424fad6
cxs <stallboy@qq.com>
2024-12-27 01:44:56 +0800

    refactor

--- app/src/main/java/configgen/i18n/LangTextFinder.java
+++ app/src/main/java/configgen/i18n/LangTextFinder.java
@@ -12,7 +12,7 @@ public class LangTextFinder {
      */
     private final Map<String, TextFinder> tableMap = new TreeMap<>();
 
-    public Map<String, TextFinder> getMap() {
+    Map<String, TextFinder> getMap() {
         return tableMap;
     }
 
