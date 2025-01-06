76e920999443baff2e53da173b0931a1edec3fe3
stallboy <stallboy@qq.com>
2025-01-06 11:53:15 +0800

    list，map里text忘了转换了，fix

--- app/src/main/java/configgen/schema/HasText.java
+++ app/src/main/java/configgen/schema/HasText.java
@@ -23,8 +23,22 @@ public class HasText {
 
         if (nameable instanceof Structural structural) {
             for (FieldSchema f : structural.fields()) {
-                if (f.type() == Primitive.TEXT) {
-                    return CheckResult.Ok;
+                switch (f.type()) {
+                    case SimpleType simpleType:
+                        if (simpleType == Primitive.TEXT) {
+                            return CheckResult.Ok;
+                        }
+                        break;
+                    case FList(SimpleType item):
+                        if (item == Primitive.TEXT) {
+                            return CheckResult.Ok;
+                        }
+                        break;
+                    case FMap(SimpleType key, SimpleType value):
+                        if (key == Primitive.TEXT || value == Primitive.TEXT) {
+                            return CheckResult.Ok;
+                        }
+                        break;
                 }
             }
         }
