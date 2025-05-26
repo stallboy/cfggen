281742897451505b217ba6c1ef72e1c729b5ece6
zhangao <ouchz@makee.com>
2025-05-26 14:52:03 +0800

    测试用例

--- example/go/config/task_completeconditiontype.go
+++ example/go/config/task_completeconditiontype.go
@@ -1,61 +1,57 @@
 package config
 
 type TaskCompleteconditiontype struct {
-    id int32 //任务完成条件类型（id的范围为1-100）
-    name string //程序用名字
+	id   int32  //任务完成条件类型（id的范围为1-100）
+	name string //程序用名字
 }
 
 func createTaskCompleteconditiontype(stream *Stream) *TaskCompleteconditiontype {
-    v := &TaskCompleteconditiontype{}
-    v.id = stream.ReadInt32()
-    v.name = stream.ReadString()
-    return v
+	v := &TaskCompleteconditiontype{}
+	v.id = stream.ReadInt32()
+	v.name = stream.ReadString()
+	return v
 }
 
-//entries
+// entries
 var (
-    killMonster TaskCompleteconditiontype
-    talkNpc TaskCompleteconditiontype
-    collectItem TaskCompleteconditiontype
-    conditionAnd TaskCompleteconditiontype
-    chat TaskCompleteconditiontype
-    testNoColumn TaskCompleteconditiontype
+	killMonster  TaskCompleteconditiontype
+	talkNpc      TaskCompleteconditiontype
+	collectItem  TaskCompleteconditiontype
+	conditionAnd TaskCompleteconditiontype
+	chat         TaskCompleteconditiontype
+	testNoColumn TaskCompleteconditiontype
 )
 
-//getters
+// getters
 func (t *TaskCompleteconditiontype) GetId() int32 {
-    return t.id
+	return t.id
 }
 
 func (t *TaskCompleteconditiontype) GetName() string {
-    return t.name
+	return t.name
 }
 
 type TaskCompleteconditiontypeMgr struct {
-    all []*TaskCompleteconditiontype
-    idMap map[int32]*TaskCompleteconditiontype
-
+	all   []*TaskCompleteconditiontype
+	idMap map[int32]*TaskCompleteconditiontype
 }
 
-func(t *TaskCompleteconditiontypeMgr) GetAll() []*TaskCompleteconditiontype {
-    return t.all
+func (t *TaskCompleteconditiontypeMgr) GetAll() []*TaskCompleteconditiontype {
+	return t.all
 }
 
-func(t *TaskCompleteconditiontypeMgr) Get(id int32) *TaskCompleteconditiontype {
-    return t.idMap[id]
+func (t *TaskCompleteconditiontypeMgr) Get(id int32) *TaskCompleteconditiontype {
+	return t.idMap[id]
 }
-
-
 
 func (t *TaskCompleteconditiontypeMgr) Init(stream *Stream) {
-    cnt := stream.ReadInt32()
-    t.all = make([]*TaskCompleteconditiontype, 0, cnt)
-    t.idMap = make(map[int32]*TaskCompleteconditiontype, cnt)
+	cnt := stream.ReadInt32()
+	t.all = make([]*TaskCompleteconditiontype, 0, cnt)
+	t.idMap = make(map[int32]*TaskCompleteconditiontype, cnt)
 
-    for i := 0; i < int(cnt); i++ {
-        v := createTaskCompleteconditiontype(stream)
-        t.all = append(t.all, v)
-        t.idMap[v.id] = v
-    }
+	for i := 0; i < int(cnt); i++ {
+		v := createTaskCompleteconditiontype(stream)
+		t.all = append(t.all, v)
+		t.idMap[v.id] = v
+	}
 }
-
--- example/go/test/test.go
+++ example/go/test/test.go
@@ -19,7 +19,7 @@ func testAllAndGet() {
 	if rawGet != get {
 		println("fail: testAllAndGet")
 	} else {
-		println("pass: testAllAndGet，But我把主键也改成BetBy的形式了")
+		println("pass: testAllAndGet")
 	}
 }
 
@@ -133,7 +133,7 @@ func testRef() {
 	cc := t.GetCompletecondition()
 	if killMonster, ok := cc.(*config.TaskCompleteconditionKillMonster); ok {
 		rawGet := config.GetOtherMonsterMgr().Get(killMonster.GetMonsterid())
-		if rawGet != killMonster.GetRefMonsterid() {
+		if rawGet != killMonster.GetRefMonsterid() || killMonster.GetRefMonsterid().GetPosList()[1].GetY() != 22 {
 			println("fail: testRef")
 		} else {
 			println("pass: testRef")
@@ -141,6 +141,91 @@ func testRef() {
 	} else {
 		println("fail: 类型不是 TaskCompleteconditionKillMonster")
 	}
+}
+
+func testRefNotCache() {
+	// local t = cfg.task.task.get(1)
+	// local refM = t.completecondition.RefMonsterid
+	// assert(refM ~= nil)
+	// assert(rawget(t.completecondition, "RefMonsterid") == nil, "Ref不会缓存，rawget一直拿到的都是nil，内存小点，这是个实现上的细节，将来可能会改变")
+	t := config.GetTaskTaskMgr().Get(1)
+	killMonster, _ := t.GetCompletecondition().(*config.TaskCompleteconditionKillMonster)
+	refM := killMonster.GetRefMonsterid()
+	if refM == nil || killMonster.GetRefMonsterid() != nil {
+		println("fail: testRefNotCache 实际上我不知道这个是在做什么")
+	} else {
+		println("pass: testRefNotCache")
+	}
+}
+
+func testNullableRef() {
+	// local t = cfg.task.task.get(1)
+	// assert(t.nexttask == 2)
+	// assert(t.NullableRefNexttask == cfg.task.task.get(2), "refType=nullable，如果为空，就==nil")
+	// assert(t.NullableRefTaskid == cfg.task.taskextraexp.get(1))
+	// t = cfg.task.task.get(3)
+	// assert(t.NullableRefNexttask == nil)
+	// assert(t.NullableRefTaskid == nil)
+	t := config.GetTaskTaskMgr().Get(1)
+	if t.GetNexttask() != 2 {
+		println("fail: testNullableRef")
+		return
+	}
+	if t.GetNullableRefNexttask() != config.GetTaskTaskMgr().Get(2) {
+		println("fail: testNullableRef")
+		return
+	}
+	if t.GetNullableRefTaskid() != config.GetTaskTaskextraexpMgr().Get(1) {
+		println("fail: testNullableRef")
+		return
+	}
+	t = config.GetTaskTaskMgr().Get(3)
+	if t.GetNullableRefNexttask() != nil {
+		println("fail: testNullableRef")
+		return
+	}
+	if t.GetNullableRefTaskid() != nil {
+		println("fail: testNullableRef")
+		return
+	}
+	println("pass: testNullableRef But,NullableRef的实现可以再优化，现在如果是nil，每次Get都会去查一次")
+}
+
+func testListRef() {
+	// local t = cfg.other.loot.get(2)
+	// assert(rawget(t, "ListRefLootid") == nil, "listRef 会缓存起来，第一次是nil")
+	// --print(t.name, t.lootid, #t.ListRefLootid, t.ListRefLootid[1].itemid, t.ListRefLootid[2].itemid)
+
+	// assert(#t.ListRefLootid == 7, "t.ListRefLootid")
+	// local itemids = {}
+	// for _, lootitem in ipairs(t.ListRefLootid) do
+	//     itemids[lootitem.itemid] = true
+	// end
+
+	// assert(itemids[40007], "t.ListRefLootid[x].itemid contains 40007")
+	// assert(itemids[40010], "t.ListRefLootid[x].itemid contains 40010")
+
+	// assert(rawget(t, "ListRefLootid") ~= nil, "listRef 会缓存起来，取过一次之后就可以直接rawget了")
+	t := config.GetOtherLootMgr().Get(2)
+	if len(t.GetListRefLootid()) != 7 {
+		println("fail: testListRef")
+		return
+	}
+	if t.GetListRefLootid()[0].GetItemid() != 22 {
+		println("fail: testListRef")
+		return
+	}
+
+	println("pass: testListRef, 我改了实现方式，现在初始化时就会构建ListRef")
+}
+
+func testEnum() {
+	// local t = cfg.task.completeconditiontype
+	// print(t.KillMonster.id, t.KillMonster.name, t.Chat.name, t.CollectItem.name)
+	// assert(t.get(1) == t.KillMonster)
+	// assert(t.KillMonster.id == 1, "配置为枚举，可以直接completeconditiontype.KillMonster访问，不用字符串")
+	// assert(t.KillMonster.name == "KillMonster")
+
 }
 
 func DoTest() {
@@ -154,4 +239,8 @@ func DoTest() {
 	testMapField()
 	testDynamicBeanField()
 	testRef()
+	testRefNotCache()
+	testNullableRef()
+	testListRef()
+	testEnum()
 }
