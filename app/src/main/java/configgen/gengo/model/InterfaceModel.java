dd56a26c775e221a3deaacbcdef30a651ed3c6cd
zhangao <ouchzsc@gmail.com>
2025-05-28 08:44:59 +0800

    jte genInterface

--- app/src/main/java/configgen/gengo/model/InterfaceModel.java
+++ app/src/main/java/configgen/gengo/model/InterfaceModel.java
@@ -7,12 +7,10 @@ public class InterfaceModel {
     public final String pkg;
     public final GoName name;
     public final InterfaceSchema sInterface;
-    public final String className;
 
     public InterfaceModel(String pkg,GoName name,InterfaceSchema sInterface) {
         this.pkg = pkg;
         this.name = name;
         this.sInterface = sInterface;
-        className = name.className;
     }
 }
